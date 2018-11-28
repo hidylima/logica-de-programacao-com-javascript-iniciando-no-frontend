85daf93208143fc2c19ea340d6a6137406f80ce3
hidylima <hidy63@gmail.com>
2018-11-28 14:13:43 -0200

    final da aula 3

--- modulo-02/aula-03-04/main.js
+++ modulo-02/aula-03-04/main.js
@@ -6,6 +6,12 @@ var number2 = prompt("digite sengundo número");
 number2 = parseFloat(number2);
 
 var media = (number1 + number2) / 2;
+var modulo = (number1 + number2) % 2;
+var divisao = (number1 / number2) ;
+var multiplicacao = (number1 * number2) ;
+var subitracao = (number1 - number2);
+number1++;
+number2--;
 
 alert("A media de  " + number1 + " + " + number2 + " : " + media);
 
---
+++ modulo-02/exercicio-02/main.js
@@ -0,0 +1,13 @@
+var number1 = prompt('Digite um nummero');
+number1 = parseInt(number1);
+
+var number2 = prompt("Digite  outro nummero");
+number2 = parseInt(number2);
+
+var substitution = number1 - number2;
+var multiplication = number1 * number2;
+var sum = number1 + number2;
+
+alert("A subritação de " + number1 + " - " + number2 + " é: " + substitution);
+alert("A multiplicação de " + number1 + " - " + number2 + " é: " + multiplication);
+alert("A soma de " + number1 + " - " + number2 + " é: " + sum);
