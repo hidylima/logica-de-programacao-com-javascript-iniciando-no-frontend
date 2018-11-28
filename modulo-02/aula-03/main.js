3c5c09ac4f877700a63c0de00ff47ef651d82a36
hidylima <hidy63@gmail.com>
2018-11-28 13:55:29 -0200

    aula 03 do modulo 2

---
+++ modulo-02/aula-03/main.js
@@ -0,0 +1,11 @@
+//perdi para usuario gigitar umm nùmero
+var number1 = prompt("digite primmeiro número");
+number1 = parseFloat(number1)
+
+var number2 = prompt("digite sengundo número");
+number2 = parseFloat(number2);
+
+var media = (number1 + number2) / 2;
+
+alert("A media de  " + number1 + " + " + number2 + " : " + media);
+
--- modulo-02/exercicio-01/main.js
+++ modulo-02/exercicio-01/main.js
@@ -6,6 +6,8 @@ number2 = parseInt(number2);
 
 var substitution = number1 - number2;
 var multiplication = number1 * number2;
+var sum = number1 + number2;
 
 alert("A subritação de " + number1 + " - " + number2 + " é: " + substitution);
 alert("A multiplicação de " + number1 + " - " + number2 + " é: " + multiplication);
+alert("A soma de " + number1 + " - " + number2 + " é: " + sum);
