549b80fd913f1cc94fc7d7fb8ca48feba96657b3
hidylima <hidy63@gmail.com>
2018-11-28 16:43:13 -0200

    exercicio 05 do modulo 02

---
+++ modulo-02/exercicio-03/main.js
@@ -0,0 +1,31 @@
+//fazer jogador1 digitar um numero
+var number1 = parseInt(prompt("jogador 1 digite um número: "));
+
+//fazer jogador2 digitar outro numero
+var number2 = parseInt(prompt("jogador 2 digite um número: "));
+
+//se forem iguais, mostrar mensagem 'empate'
+if (number1 === number2) {
+    alert("Deu empate")
+} else if (number1 > number2) {
+    alert("o número " + number1 + " è maior ");
+} else {
+    alert("o número " + number2 + " è maior ");
+}
+//sortear um numero entre 0 e 1
+var sorteio = parseInt(Math.random() * 2);
+// se o numero sorteado for 0, ganha quem escolher o numero MENOR
+if (sorteio == 0) {
+    if (number1 < number2) {
+        alert("jogador 1  " + number1 + " ganhou ");
+    } else {
+        alert("jogador 2  " + number2 + " ganhou  ");
+    }
+} else if (sorteio == 1) {
+    if (number1 > number2) {
+        alert("jogador 1  " + number1 + " ganhou  ");
+    } else {
+        alert("jogador 3  " + number2 + " ganhou  ");
+    }
+}
+//se o numero sorteado for 1, ganha quem escolher o numero MAIOR
